882f2d66215c49ad6cefb66802110f1079820514
Marcin Rudowski <marcin.rudowski@fingo.info>
2020-07-10 00:51:16 +0200

    testing hadncrafted translation as object for stron typing etc

--- src/engine/VerbMenu/utils/getTopLeftPosition.ts
+++ src/engine/VerbMenu/utils/getTopLeftPosition.ts
@@ -1,11 +1,11 @@
 const getTopLeftPosition = (x: number, y:number, childRect: DOMRect, parentRect: DOMRect) => {
   const marginSize = 15;
-
-  let left = Math.min(x - childRect.x | marginSize);
+  //console.log('%c [mr]', 'background-color:Gold; color: black', x , parentRect, window.pageXOffset, document.documentElement.clientLeft);
+  let left = Math.min(x - parentRect.x - window.pageXOffset | marginSize);
   if (left + childRect.width + marginSize > parentRect.width) {
     left = parentRect.width - childRect.width - marginSize; // or right?
   }
-  let top = Math.min(y - childRect.y | marginSize);
+  let top = Math.min(y - parentRect.y - window.pageYOffset | marginSize);
   if (top + childRect.height + marginSize > parentRect.height) {
     top = parentRect.height - childRect.height - marginSize; // or bottom?
   }
